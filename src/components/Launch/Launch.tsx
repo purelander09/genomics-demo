f4b181724c3beb65814e31107eecec497d141bdf
Ryan Boone <ryan.a.boone@accenture.com>
2019-04-19 12:08:28 -0500

    Initial commit of code

---
+++ src/components/Launch/Launch.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Launch = () => {
+  
+};
+
+export default Launch;
---
+++ src/routes.tsx
@@ -0,0 +1,7 @@
+import { Route } from 'react-router-dom';
+
+export const Routes = [
+  <Route exact={true} path="/launch" component={Launch} />
+];
+
+export default Routes;
