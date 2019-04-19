fe7faaa0def6df444ff727bb7ce74ab41f23e578
Ryan Boone <ryan.a.boone@accenture.com>
2019-04-19 13:01:19 -0500

    Adding editorconfig

--- src/components/Launch/Launch.tsx
+++ src/components/Launch/Launch.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export const Launch = () => {
-  
+  return (<div />);
 };
 
 export default Launch;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import { Route } from 'react-router-dom';
 
+import { Launch } from './components/Launch';
+
 export const Routes = [
   <Route exact={true} path="/launch" component={Launch} />
 ];
